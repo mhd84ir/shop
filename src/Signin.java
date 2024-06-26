7a09399fe3e5b91fcb7fb5ba97eebed9444618a7
mwri-rs <mariarostami.a1384@gmail.com>
2024-06-26 20:31:29 +0330

    Signin class added.

---
+++ src/Signin.java
@@ -0,0 +1,14 @@
+public class Signin {
+
+    public boolean checkUsername (String username)
+    {
+        //TODO
+        return true;
+    }
+
+    public boolean checkPassword (String password)
+    {
+        //TODO
+        return true;
+    }
+}
