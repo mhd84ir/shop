e9fb8290b2f263dce17c1d944d61d41f71b3da1e
mhd84ir <mhd84.ir@gmail.com>
2024-06-30 15:53:10 +0330

    bug fixed.

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -71,8 +71,8 @@ public class LoginForm extends JFrame implements ActionListener {
         // Creating right panel for image
         JPanel rightPanel = new JPanel();
         //Your file's name should  be based on your own OS
-        JLabel imageLabel = new JLabel(new ImageIcon("/Users/mhd84.ir/uni/Shop-Project/src/photos/pexels-photo-3593865.png"));
-//        JLabel imageLabel = new JLabel(new ImageIcon("C:\\Users\\asus\\Desktop\\java\\AP\\shop\\src\\photos\\pexels-photo-3593865.png"));
+//        JLabel imageLabel = new JLabel(new ImageIcon("/Users/mhd84.ir/uni/Shop-Project/src/photos/pexels-photo-3593865.png"));
+        JLabel imageLabel = new JLabel(new ImageIcon("C:\\Users\\asus\\Desktop\\java\\AP\\shop\\src\\photos\\pexels-photo-3593865.png"));
         rightPanel.add(imageLabel);
 
         // Adding panels to frame
